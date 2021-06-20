6be507ef7ce7246df105505bae3ff43cfe4c80f3
Debojeet Jha <debojeetjha@gmail.com>
2021-06-20 12:14:48 +0530

    removed port for deployment in new peer()

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: 'debo-peerjs.herokuapp.com',
-  port: '80'
+  port: ''
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
